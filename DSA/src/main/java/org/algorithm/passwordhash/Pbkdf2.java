60841ee193a3f7dbcbce869367e53a2bd98568fe
kuyeol <zimop12@gmail.com>
2024-08-01 13:26:00 +0900

    a

---
+++ DSA/src/main/java/org/algorithm/passwordhash/Pbkdf2.java
@@ -0,0 +1,45 @@
+package org.algorithm.passwordhash;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import org.apache.commons.codec.binary.Hex;
+import org.keycloak.common.util.Base64;
+
+public class Pbkdf2 {
+
+  public static void main(String[] args) throws
+      IOException {
+
+    String password = "admin";
+    String salt = "8AW6obr8/CoQD2ih/DJnOw==";
+    int iterations = 27500;
+    int keyLength = 256;
+    char[] passwordChars = password.toCharArray();
+    byte[] saltBytes = Base64.decode(salt);
+
+    byte[] hashedBytes = hashPassword(passwordChars, saltBytes, iterations, keyLength);
+    String hashedString = Hex.encodeHexString(hashedBytes);
+
+    System.out.println(hashedString);
+  }
+
+  public static byte[] hashPassword( final char[] password, final byte[] salt, final int iterations, final int keyLength ) {
+
+    try {
+      SecretKeyFactory skf = SecretKeyFactory.getInstance( "PBKDF2WithHmacSHA256" );
+      PBEKeySpec spec = new PBEKeySpec( password, salt, iterations, keyLength );
+      SecretKey key = skf.generateSecret( spec );
+      byte[] res = key.getEncoded( );
+      return res;
+    } catch ( NoSuchAlgorithmException | InvalidKeySpecException e ) {
+      throw new RuntimeException( e );
+    }
+  }
+}
